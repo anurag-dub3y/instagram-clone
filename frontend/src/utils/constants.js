7133726a1d1f6fd843b106b53a52e68c25c5cd66
Anurag Dubey <anuragdubeyisawesome@gmail.com>
2023-07-15 00:29:27 +0530

    Initial Commit

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,60 @@
+export const BASE_PROFILE_IMAGE_URL='http://localhost:3001/public/uploads/profiles/';
+export const BASE_POST_IMAGE_URL='http://localhost:3001/public/uploads/posts/';
+
+export const SOCKET_ENDPOINT = "http://localhost:3001";
+
+
+export const stories = [
+    {
+        title: "JavaScript",
+        image: "javascript"
+    },
+    {
+        title: "Node.js",
+        image: "nodejs"
+    },
+    {
+        title: "Express.js",
+        image: "expressjs"
+    },
+    {
+        title: "MongoDB",
+        image: "mongodb"
+    },
+    {
+        title: "React.js",
+        image: "reactjs"
+    },
+    {
+        title: "Socket.io",
+        image: "socketio"
+    },
+    {
+        title: "TailwindCSS",
+        image: "tailwind"
+    },
+    {
+        title: "MaterialUI",
+        image: "mui"
+    },
+    {
+        title: "Redux",
+        image: "redux"
+    },
+    {
+        title: "Multer",
+        image: "multer"
+    },
+    {
+        title: "Sendgrid",
+        image: "sendgrid"
+    },
+    {
+        title: "Axios",
+        image: "axios"
+    },
+    {
+        title: "Toastify",
+        image: "toastify"
+    },
+]
